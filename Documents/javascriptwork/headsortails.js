138ed2e4d77ee9ea6aec5954a300e092a10396ec
Jere Karvonen <t9kaje02@students.oamk.fi>
2019-09-23 11:53:21 +0300

    first commit

---
+++ Documents/javascriptwork/headsortails.js
@@ -0,0 +1,29 @@
+
+// Heads or tails
+process.stdout.write( "\n Pick Heads or Tails."
++ " \n This code will flip a coin for you " ) ;
+
+process.stdin.on("data",function(input_from_user)  {};
+
+var coinFlip = Math.floor(Math.random)* 1 ;
+
+var choice = (prompt "Heads or Tails") ;
+
+if (coinFlip == 1) { 
+	var flipResult = "Heads" ; 
+} else { 
+	var flipResult = " Tails " ;
+}
+if choice = " input_from_user" 
+{
+	process.stdout.write( "\n You picked the right side, you win ! ) " ;
+	}
+	{ else 
+		process.stdout.write( "\n You picked the wrong side, you lose! " ) ;
+	}
+	
+	process.exit() ;
+
+} ) ; 
+
+// skfslfsd
